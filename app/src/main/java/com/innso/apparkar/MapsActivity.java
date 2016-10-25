2b565a85936a25213b8e85fc89ca6c8b1213f82e
andrespaez90 <andres.paez@innso.co>
2016-10-25 17:07:59 -0500

    put market example

--- app/src/main/java/com/innso/apparkar/MapsActivity.java
+++ app/src/main/java/com/innso/apparkar/MapsActivity.java
@@ -56,6 +56,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         LatLng sydney = new LatLng(-34, 151);
 
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
         mMap.setOnCameraMoveListener(() -> {
             if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_EXPANDED) {
                 bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
